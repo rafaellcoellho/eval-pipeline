4fcb3f196a8e56e687efa1b86ceee844a7e7b673
Rafael Coelho <rafaellcoellho@gmail.com>
2026-06-01 10:38:47 -0300

    chore: remove sessions execution screen

--- eval_pipeline/app.py
+++ eval_pipeline/app.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
-from eval_pipeline.routers import analysis, golden_cases, index, run, sessions
+from eval_pipeline.routers import analysis, golden_cases, index, run
 
 STATIC_PATH = Path(__file__).parent.parent / "static"
 
@@ -12,6 +12,6 @@ app = FastAPI(title="eval pipeline")
 app.mount("/static", StaticFiles(directory=STATIC_PATH), name="static")
 app.include_router(index.router)
 app.include_router(golden_cases.router)
-app.include_router(sessions.router)
+
 app.include_router(run.router)
 app.include_router(analysis.router)
